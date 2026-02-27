4bacdc2fb02a4ca5bf62023fd15160fbb8a1a9b4
Mohamed Emad <119817795+MohamedEmad223@users.noreply.github.com>
2026-02-27 14:03:31 +0200

    - finish over_view_data_source.dart.dart

---
+++ lib/features/home/data/data_source/over_view_data_source.dart
@@ -0,0 +1,18 @@
+import 'package:dashboard_for_url_shortner/core/networking/end_points.dart';
+import 'package:dashboard_for_url_shortner/features/home/data/model/over_view_response_model.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+
+import '../../../../core/networking/api_constants.dart';
+
+part 'over_view_data_source.g.dart';
+
+@RestApi(baseUrl: ApiConstants.baseUrl)
+abstract class OverViewDataSource {
+  factory OverViewDataSource(Dio dio, {String baseUrl}) = _OverViewDataSource;
+
+  @GET(EndPoints.overView)
+  Future<OverViewResponseModel> overView(
+  );
+}
